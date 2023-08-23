f9090043faebba1ef3751f6975d55abf7207eac1
BalabanP <pbalaban6@gmail.com>
2023-08-23 11:41:03 +0300

    add comments

--- app/fun_facts/fun_fact_service.py
+++ app/fun_facts/fun_fact_service.py
@@ -3,6 +3,8 @@ import requests
 urls = {"fun_data": "http://numbersapi.com/{month}/{day}/date"}
 
 
+# if are more then one api calls is better to use async aproch
+# as for this exemple is not worth
 def get_fun_fact(month, day):
     try:
         url = urls.get("fun_data").format(month=month, day=day)
